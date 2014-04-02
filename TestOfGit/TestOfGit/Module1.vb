831b5091f2efeb6ab5593346e10cc4826b5d5469
Lab User <saipriya02@hotmail.com>
2014-04-02 14:12:38 -0700

    Test

---
+++ TestOfGit/TestOfGit/Module1.vb
@@ -0,0 +1,7 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("hello world")
+    End Sub
+
+End Module
---
+++ TestOfGit/TestOfGit/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TestOfGit")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("TestOfGit")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cb464936-5ca9-4f94-943f-992de86c47c3")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
